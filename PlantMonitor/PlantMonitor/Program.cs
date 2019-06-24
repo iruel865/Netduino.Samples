124bcaf9cfc9dd0a3444d5974eee753a10909484
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-06-23 20:26:21 -0700

    [PlantMonitor] Initial Check-in

---
+++ PlantMonitor/PlantMonitor/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.SPOT;
+using System.Threading;
+
+namespace PlantMonitor
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Hello World!");
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
