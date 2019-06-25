c4d78fc7c61a73b8744f9b3c6e084587c7d9a97a
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-06-24 23:13:16 -0700

    [PlantMonitor] Add HumiditySensorController

---
+++ PlantMonitor/PlantMonitor/HumiditySensorController.cs
@@ -0,0 +1,37 @@
+using MSH = Microsoft.SPOT.Hardware;
+using SLH = SecretLabs.NETMF.Hardware;
+using System.Threading;
+
+namespace PlantMonitor
+{
+    public class HumiditySensorController
+    {
+        protected SLH.AnalogInput _analogPort;
+        protected MSH.OutputPort _digitalPort;
+
+        public HumiditySensorController(MSH.Cpu.Pin analogPort, MSH.Cpu.Pin digitalPort)
+        {
+            _analogPort = new SLH.AnalogInput(analogPort);
+            _digitalPort = new MSH.OutputPort(digitalPort, false);
+        }
+
+        public float Read()
+        {
+            int sample;
+            float humidity;
+
+            _digitalPort.Write(true);
+            Thread.Sleep(5);
+            sample = _analogPort.Read();
+            _digitalPort.Write(false);
+
+            humidity = 100 - Map(sample, 250, 1023, 0, 100);
+            return humidity;
+        }
+
+        protected float Map(float value, float fromLow, float fromHigh, float toLow, float toHigh)
+        {
+            return (((toHigh - toLow) * (value - fromLow)) / (fromHigh - fromLow)) - toLow;
+        }
+    }
+}
