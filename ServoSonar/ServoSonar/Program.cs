5bcd1835c648ec349b271cd6d90eec8c244a04c2
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-07-27 00:35:30 -0700

    [ServoSonar] Initial Check-in

---
+++ ServoSonar/ServoSonar/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.SPOT;
+
+namespace ServoSonar
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Hello World");
+        }
+    }
+}
